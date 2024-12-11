a279ef24f2cf21cdb43c9d1f126d2777a209b19a
BlackT1nkerBell <paigevbell@gmail.com>
2024-12-10 23:42:39 -0500

    Update drumming_Controls.ino
    Added notes

--- drumming_Controls.ino
+++ drumming_Controls.ino
@@ -8,34 +8,22 @@ void setup() {
 }
 
 void loop() {
+
+//X, Y, and Z coordinates of the playground respecfully are given a variable
   X = CircuitPlayground.motionX();
   Y = CircuitPlayground.motionY();
   Z = CircuitPlayground.motionZ();
 
-  // Serial.print("X: ");
-  // Serial.print(X);
-  // Serial.print("  Y: ");
-  // Serial.print(Y);
-  // Serial.print("  Z: ");
-  // Serial.println(Z);
-
+//Detects cymbal hit if the playground has an acceleration of 19 in the negative Z direction
   if (Z <= -19) {
   Serial.print("Cymbal Hit!");
   delay(50);
 }
 
+//Detects drum hit if the playground has an acceleration of 19 in the negative X direction
   if (X <= -19) {
   Serial.print("Drum Hit!");
   delay(50);
 }
 
-//   if (Z >= 19) {
-//   Serial.print("Cymbal Hit!");
-// }
-
-//   if (X >= 18) {
-//   Serial.print("Drum Hit!");
-// }
-
-//  delay(50);
 }
